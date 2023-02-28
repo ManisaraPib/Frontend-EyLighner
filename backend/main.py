c34a3d30ce7f9a4879f478755e81fd44622e006c
GutjungPPJ <91058560+GutjungPPJ@users.noreply.github.com>
2023-02-28 12:45:19 +0700

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -34,50 +34,36 @@ CORS(app)
 CORS(app, resources={r'/*': {'origins': '*'}},CORS_SUPPORTS_CREDENTIALS = True)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
-#Flask run test page
-@app.route("/test") # Define route of the function
-def test_page():
-    return "Flask run sucess"
 
 #Upload 
-@app.route('/input/', methods=['GET', 'POST'])
+@app.route('/home/', methods=['GET', 'POST'])
 def upload_file():
 
     # Get post method from frontend
     if request.method == 'POST':
+        print("uploading...")
+
+        # Get request file from frontend
+        number = request.form.get("number")
+        openeye_image = request.files['open_image']
+        closeeye_image = request.files['close_image']
+        
         # Check if file not exist
         if 'open_image' not in request.files:
             return 'open_image is not exist'
         if 'close_image' not in request.files:
             return 'close_image is not exist'
 
-        # Step
-        # rename images follow by image description (api topic)
-
-        # Get request file from frontend
-        number = request.form.get("number")
-        openeye_image = request.files['open_image']
-        closeeye_image = request.files['close_image']
 
         # Save file to givened directoery
         openeye_path = os.path.join(app.config['UPLOAD_FOLDER'], f"{str(number)}open_eye.jpg")
         closeeye_path = os.path.join(app.config['UPLOAD_FOLDER'], f"{str(number)}close_eye.jpg")
         openeye_image.save(openeye_path)
         closeeye_image.save(closeeye_path)
-        return render_template(('input.html'))
-
-    return render_template(('input.html'))
-
-# @app.route('/get_image')
-# def get_image():
-
-#     image
-#     # return send_file(filename, mimetype='image/gif')
-#     return redirect(url_for(".home", image = image))
+        print ("Save images")
 
 #Select Image from Folder
-@app.route("/home/", methods = ["GET","POST"])
-def home():
+def select():
     if request.method == "POST":
         image_name = request.form.get("fname")
         print(image_name)
@@ -89,18 +75,6 @@ def home():
 
         image_path = os.path.join(app.config['UPLOAD_FOLDER'], f"{file_name}.jpg")
         print(image_path)
-
-        # img = cv2.imread('./image_storage/1open_eye.jpg',0)
-        # img = Image.open(image_path)
-        # print(img)
-
-        # return redirect(url_for(".home"))
-        return send_file(image_path, mimetype='image/gif')
-
-
-    return render_template("home.html")
-
-
 
 
 #Contact Page
